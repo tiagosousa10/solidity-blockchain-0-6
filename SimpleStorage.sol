ef0f6756154e80af263157871b39777553f4cea5
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-08 22:14:57 +0100

    structures and arrays example

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,15 +4,26 @@ pragma solidity 0.8.18;
 contract SimpleStorage {
     //favoriteNumber gets initialized to 0 if no value is given
 
-    uint256 public favoriteNumber; //0
+    uint256 myFavoriteNumber; //0
+
+    //uint256[] listOfFavoritesNumbers; 
+    // 0  1  2
+    // [0,78,90]
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person public pat = Person({favoriteNumber: 7, name: "pat"});
+    Person public mariah = Person({favoriteNumber: 3, name: "mariah"});
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     //view -> read state (dont update) || pure 
     function retrieve() public view returns (uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 
 }
