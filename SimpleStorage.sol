6ef0a30111cbf4f6d16b7bad4529fa5351adfaa1
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-09 14:09:29 +0100

    testing new examples of contracts, and interact with them

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,46 +1,39 @@
+// I'm a comment!
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+
+pragma solidity 0.8.19;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-    //favoriteNumber gets initialized to 0 if no value is given
+    uint256 myFavoriteNumber;
 
-    uint256 myFavoriteNumber; //0
-
-    //uint256[] listOfFavoritesNumbers; 
-    // 0  1  2
-    // [0,78,90]
     struct Person {
         uint256 favoriteNumber;
         string name;
     }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
 
-    //dynamic array
-    Person [] public listOfPeople; // [] empty list || [3] -> static array
-
-//    Person public pat = Person({favoriteNumber: 7, name: "pat"});
-  //  Person public mariah = Person({favoriteNumber: 3, name: "mariah"});
-
-// chelsea -> 232
     mapping(string => uint256) public nameToFavoriteNumber;
 
-
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         myFavoriteNumber = _favoriteNumber;
     }
 
-    //view -> read state (dont update) || pure 
     function retrieve() public view returns (uint256) {
         return myFavoriteNumber;
     }
 
-//calldata (you CANT modify), memory(you can modify) -> temporary variables     || storage 
-    function addPerson(string memory _name, uint256 _favoriteNumber ) public {
-        //_name = "cat";
-        
-        //Person memory newPerson = Person(_favoriteNumber, _name)
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
-
-        nameToFavoriteNumber[_name] =  _favoriteNumber;
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
 }
+
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
