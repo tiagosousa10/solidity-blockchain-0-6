58103dc7675dcdef191bc7e40d3ec251ff5d6bfd
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-10 13:57:35 +0100

    exercises with arrays, Storage Factory functions

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,33 +1,21 @@
 // SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
-pragma solidity ^0.8.19;
-
-// import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
 import {SimpleStorage} from "./SimpleStorage.sol";
 
-contract StorageFactory {
+contract StorageFactory{
+
     SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorageContractVariable = new SimpleStorage();
-        // SimpleStorage simpleStorage = new SimpleStorage();
-        listOfSimpleStorageContracts.push(simpleStorageContractVariable);
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
     }
 
-    function sfStore(
-        uint256 _simpleStorageIndex,
-        uint256 _simpleStorageNumber
-    ) public {
-        // Address
-        // ABI
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
-        listOfSimpleStorageContracts[_simpleStorageIndex].store(
-            _simpleStorageNumber
-        );
-    }
-
-    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
-        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        //address
+        //ABI - Application Binary Interface
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
     }
 }
