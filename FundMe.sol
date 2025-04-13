68b74d83071fce00f07dcabd5ec8432a3385fdcb
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-13 21:49:09 +0100

    declaring a minimum usd to send money

--- FundMe.sol
+++ FundMe.sol
@@ -4,13 +4,16 @@ pragma solidity 0.8.19;
 contract FundMe {
     constructor() payable  {}
 
-    uint256 public myValue = 1;
+    //uint256 public myValue = 1;
+    uint public minimumUsd = 5;
 
     function fund() public payable {
-        // allow users to send money
-        //have a minimum $ sent
-        //1. how do we send eth to this
-    myValue = myValue + 2;
-    require(msg.value >= 1e18, "didn't send enough ether");
+    // allow users to send money
+    //have a minimum $ sent
+    //1. how do we send eth to this
+    //myValue = myValue + 2;
+    require(msg.value >= minimumUsd, "didn't send enough ether");
+
+
     }
 }
