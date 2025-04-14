ccefcd9c97ef868cc827deb57be93b43f4300918
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-14 20:04:52 +0100

    getConversionRate with an ethAmount

--- FundMe.sol
+++ FundMe.sol
@@ -7,14 +7,14 @@ contract FundMe {
     constructor() payable  {}
 
     //uint256 public myValue = 1;
-    uint public minimumUsd = 5;
+    uint public minimumUsd = 5e18;
 
     function fund() public payable {
     // allow users to send money
     //have a minimum $ sent
     //1. how do we send eth to this
     //myValue = myValue + 2;
-    require(msg.value >= minimumUsd, "didn't send enough ether");
+    require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ether");
 
     }
 
@@ -29,7 +29,14 @@ contract FundMe {
         return uint256(price) * 1e10;
     }
 
-    function getConversationRate() public {}
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        // 1 ETH ?
+        //1600_00000000(18zeros)
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+
+        return ethAmountInUsd;
+    }
 
     function getVersion() public view returns(uint256) {
         return AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43).version();
