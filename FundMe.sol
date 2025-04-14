8a842bf74edaa223df11be8f412c35d693e1934e
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-14 19:57:45 +0100

    get price with aggregatorv3Interface from chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -18,9 +18,15 @@ contract FundMe {
 
     }
 
-    function getPrice() public{
+    function getPrice() public view returns (uint256){
         //address 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
         //abi 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        (, int256 price,,, )= priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        // 1600
+
+        return uint256(price) * 1e10;
     }
 
     function getConversationRate() public {}
