89966b08d569d0814c7ec77ffcd544eb9565f627
tiagosousa10 <tiagosousa.tams@hotmail.com>
2025-04-16 15:24:34 +0100

    for loop examples

--- FundMe.sol
+++ FundMe.sol
@@ -16,8 +16,20 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ether");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
 
+    function withdraw() public{
+        // for loop
+        // [1,2,3]
+        //for(/*starting index, ending index, step amount */)
+        //0, 10,1
+        //0,1,2,3,4
+
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
     }
 
     
